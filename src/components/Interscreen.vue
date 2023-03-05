9cbf14cd47a8bee57b6016781da3e6358118bf41
Alison Delvechio <alisondelvechio@outlook.com>
2023-03-05 11:56:21 -0300

    Component e install firebase

---
+++ src/components/Interscreen.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <v-container>
+      <v-row>
+        <div class="fill-height">
+          <v-btn block>
+            Block Button
+          </v-btn>
+        </div>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Interscreen',
+
+  data () {
+    return {
+        
+    }
+  },
+})
+</script>
---
+++ src/firebase/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCDHze8ytRfLwzxVMOlJrFR_S79WrSSokU',
+  authDomain: 'fast-acaiteria.firebaseapp.com',
+  projectId: 'fast-acaiteria',
+  storageBucket: 'fast-acaiteria.appspot.com',
+  messagingSenderId: '932114200119',
+  appId: '1:932114200119:web:6a900b2dba4271ec1631b2'
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
